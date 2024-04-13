b1305367195fc699752ce7f2a82b206f8382aff6
Abel Santillan Rodriguez <diversia.abel@gmail.com>
2024-04-12 20:56:15 -0600

    removed certain fno implementations and moved into layers

--- FNO/__init__.py
+++
@@ -1,5 +0,0 @@
-from .fno_1d_time import FNO1DTime
-from .fno_2d_time import FNO2DTime
-from .fno_3d_time import FNO3DTime
-from . import lploss
-from .train import train_model
--- FNO/layers/__init__.py
+++
@@ -1,3 +0,0 @@
-from .fno_block import FourierBlock
-from .mlp import MLP
-from .spectral_convolution import SpectralConvolution
