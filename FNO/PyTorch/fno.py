aba70caf190de99d8fdc30cdd7c26d4e53d0f58b
Abel Santillan Rodriguez <diversia.abel@gmail.com>
2024-05-20 22:31:04 -0600

    Refactor FNO module to include lifting and projection channels in PyTorch implementation

--- FNO/PyTorch/fno.py
+++ FNO/PyTorch/fno.py
@@ -8,7 +8,7 @@ class FNO(nn.Module):
     """
     FNO (Fourier Neural Operator) model for solving PDEs using deep learning.
     """
-    def __init__(self, modes: List[int], num_fourier_layers: int, in_channels: int, out_channels: int, mid_channels: int, activation: nn.Module, **kwargs: bool):
+    def __init__(self, modes: List[int], num_fourier_layers: int, in_channels: int, lifting_channels: int, projection_channels:int, out_channels: int, mid_channels: int, activation: nn.Module, **kwargs: bool):
         """
         Initialize the FNO model.
 
@@ -16,6 +16,7 @@ class FNO(nn.Module):
             modes (List[int]): List of integers representing the number of Fourier modes along each dimension.
             num_fourier_layers (int): Number of Fourier blocks to use in the model.
             in_channels (int): Number of input channels.
+            lifting_channels (int): Number of channels in the lifting layer.
             out_channels (int): Number of output channels.
             mid_channels (int): Number of channels in the intermediate layers.
             activation (nn.Module): Activation function to use.
@@ -32,6 +33,8 @@ class FNO(nn.Module):
         self.in_channels = in_channels
         self.out_channels = out_channels
         self.mid_channels = mid_channels
+        self.lifting_channels = lifting_channels
+        self.projection_channels = projection_channels
         self.activation = activation
         self.add_grid = kwargs.get('add_grid', False)
         self.padding = kwargs.get('padding', None)
@@ -50,17 +53,22 @@ class FNO(nn.Module):
             
 
         # Lifting layer (P)
-        self.p = nn.Linear(self.in_channels + (self.dim if self.add_grid else 0), self.mid_channels)
+        if self.lifting_channels is not None:
+            self.p1 = nn.Linear(self.in_channels + (self.dim if self.add_grid else 0), self.lifting_channels)
+            self.p2 = nn.Linear(self.lifting_channels, self.mid_channels)
+        else:
+            self.p1 = nn.Linear(self.in_channels + (self.dim if self.add_grid else 0), self.mid_channels)
+        
 
         # Fourier blocks
         self.fourier_blocks = nn.ModuleList([
-            FourierBlock(modes, mid_channels, mid_channels, out_channels, activation)
+            FourierBlock(modes, mid_channels, mid_channels, activation=activation)
             for _ in range(num_fourier_layers)
         ])
 
         # Projection layer (Q)
-        self.q = nn.Linear(self.mid_channels,self.mid_channels)
-        self.final = nn.Linear(self.mid_channels, self.out_channels)
+        self.q1 = nn.Linear(self.mid_channels,self.projection_channels)
+        self.final = nn.Linear(self.projection_channels, self.out_channels)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
@@ -80,17 +88,16 @@ class FNO(nn.Module):
 
         # If grid is enabled, set the grid
         if self.add_grid:
-            self.set_grid(x)
             for i in range(len(sizes)):
-                if sizes[i] != self.sizes[i]:
+                if sizes[i] != self.sizes[i] or self.grids[0].shape[0] != batch:
+                    self.set_grid(x)
                     break
-            reshapes = [1] * len(x.size())
-            reshapes[0] = batch
-            batched_grid = [grid.repeat(reshapes) for grid in self.grids]
-            x = torch.cat((*batched_grid, x), dim=-1)
+            x = torch.cat((x, self.grids), dim=-1)
 
         # Lifting layer
-        x = self.p(x)
+        x = self.p1(x)
+        if self.lifting_channels is not None:
+            x = self.p2(x)
 
         # Permute the dimensions [batch, *sizes, channels] -> [batch, channels, *sizes]
         x = x.permute(0, -1, *range(1, self.dim + 1))
@@ -111,7 +118,7 @@ class FNO(nn.Module):
         x = x.permute(0, *range(2, self.dim + 2), 1)
 
         # Projection layer
-        x = self.q(x)
+        x = self.q1(x)
 
         # Activation
         x = self.activation(x)
@@ -139,5 +146,9 @@ class FNO(nn.Module):
             new_shape[dim + 1] = sizes[dim]
             repeats = [1] + sizes + [1]
             repeats[dim + 1] = 1
-            grid = torch.linspace(0, 1, sizes[dim], requires_grad=True, device=x.device, dtype=torch.float).reshape(new_shape).repeat(repeats).clone().detach()
+            repeats[0] = batch
+            grid = torch.linspace(0, 1, sizes[dim], device=x.device, dtype=torch.float).reshape(*new_shape).repeat(repeats)
             self.grids.append(grid)
+        
+        self.grids = torch.cat(self.grids, dim=-1)
+        
